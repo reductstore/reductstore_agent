0cf6f720f915b203a33a1b195e4a89d11d1fda88
anthonycvn <anthony.cavin.ac@gmail.com>
2025-06-07 13:19:59 +0200

    missing copywrites

--- test/test_copyright.py
+++ test/test_copyright.py
@@ -16,10 +16,6 @@ import pytest
 from ament_copyright.main import main
 
 
-# Remove the `skip` decorator once the source file(s) have a copyright header
-@pytest.mark.skip(
-    reason="No copyright header has been placed in the generated source file."
-)
 @pytest.mark.copyright
 @pytest.mark.linter
 def test_copyright():
