c79882ede5513a50ee1e1f58b6afe6cfc66a874b
Hung Nguyen <hung@reduct.store>
2025-11-06 11:46:02 +0000

    changed logic of rawoutput class

--- reductstore_agent/raw_output.py
+++ reductstore_agent/raw_output.py
@@ -20,54 +20,99 @@
 
 """Raw output format logic per pipeline."""
 
+
 from rclpy.serialization import serialize_message
-from reductstore_agent.recorder import Recorder
 from reductstore_agent.config_models import PipelineConfig
-from reduct import Client
 from typing import Any
+import io
+from tempfile import SpooledTemporaryFile
+import json
+from .utils import ns_to_us, ros2_type_name
+import struct
+
+
 BATCH_LIMIT = 100 * 1024
-
-
-#4. decide if we batch or stream data depending on record size:
-# batch if record < 100KB
-# meaning u put it into the buffer 
-# streaming = send it immediately
-
-
-
-# Possible workflow
-#1. serialize data via serialization
-#2. decide to either batch or stream data
-#2. save the data
-#3. send http call to reduct client 
+FRAME_HEADER = struct.Struct("<QII")
 
 
 class RawOutputWriter:
-    def __init__(self, reduct_client: Client, entry_name:str):
-        self.client = reduct_client
+    def __init__(
+            self,
+            bucket, 
+            buffer: SpooledTemporaryFile[bytes],
+            pipeline_name: PipelineConfig,
+            # spool_max_size: int
+    ):
+        self.bucket = bucket
+        self.buffer = buffer
+        # self.spool_max_size = spool_max_size
+        self.pipeline_name = pipeline_name
 
 
-    def write_message(self, message: Any, topic_type: str, publish_time: int):
-        # 1. serialize data 
-        
+    async def upload_to_reductstore(self, pipeline_name, serialized_data: bytes, timestamp: int, labels):
+        await self.client.bucket.write(
+            pipeline_name,
+            timestamp, # use as index
+            serialized_data,
+            labels
+        )
+
+
+    def write_message(self, message: Any, publish_time: int):
         try: 
             serialized_data = serialize_message(message)
         except:
             return
         
         # 2. Add labels timestamp etc
-        # use timestamp in ms as index
-
+        timestamp_ms =  ns_to_us(publish_time)
+        topic_type = ros2_type_name(message)
         labels = {
             "type": f"{topic_type} Message",
             "serialization": "cdr",
         }
-        timestamp_ms =  publish_time // 1000
+        record_size = len(serialized_data)
+        if record_size >= self.BATCH_LIMIT:
+            # Upload because streaming
+            # Flush and upload buffer first and then stream this
+            self.flush_buffer(self.buffer)
+            # Do we combine the data first ?
+            # Because if we do it like this, two calls are made
+            self.upload_to_reductstore(self.pipeline_name, serialized_data, timestamp_ms, labels)
+        
+        # If smaller than 100KB batch the record into buffer
+        self.append_record(self.buffer, timestamp_ms, labels, serialized_data)
 
 
-    async def upload_to_reductstore():
-        pass
+def append_record(self, buffer: SpooledTemporaryFile, ts_us: int, labels: dict, serialized_data: bytes) -> None:
+    labels_bytes = json.dumps(labels, separators=(",", ":")).encode("utf-8")
+    # Have to add a header for the buffer, to later extract data out of it
+    hdr = self.FRAME_HEADER.pack(ts_us, len(labels_bytes), len(serialized_data))
 
+    buffer.seek(0, io.SEEK_END)
+    buffer.write(hdr)
+    buffer.write(labels_bytes)
+    buffer.write(serialized_data)
+
+
+def flush_buffer(self, buffer: SpooledTemporaryFile):
+    buffer.seek(0)
+    read = buffer.read
+    while True:
+        hdr = read(self.FRAME_HEADER.size)
+        if not hdr:
+            break
+        timestamp_ms, l_len, p_len = self.FRAME_HEADER.unpack(hdr)
+        labels = json.loads(read(l_len).decode("utf-8"))
+        serialized_data = read(p_len)
+
+        self.upload_to_reductstore(self.pipeline_name, serialized_data, timestamp_ms, labels)
+
+    buffer.seek(0)
+    buffer.truncate(0)
+    
+
+    
 
 
 
--- reductstore_agent/utils.py
+++ reductstore_agent/utils.py
@@ -77,3 +77,14 @@ def parse_bytes_with_si_units(value: str | int | float) -> int:
         raise ValueError(f"Byte value must be non-negative, got {result}")
 
     return result
+
+
+def ns_to_us(ns: int) -> int:
+    """Round nanoseconds to microseconds."""
+    return int(round(ns / 1_000.0))
+
+
+def ros2_type_name(msg) -> str:
+    """Get Topic Names of messages."""
+    mod = msg.__class__.__module__.replace(".", "/")
+    return f"{mod}/{msg.__class__.__name__}"
